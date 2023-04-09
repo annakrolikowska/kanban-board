20846b4e8b326f249c352928aa8a042382b97692
Anna <krolikowska.ann@gmail.com>
2023-04-09 21:35:33 +0200

    added confirmation dialog (column limit) to column component

--- src/components/Column.js
+++ src/components/Column.js
@@ -1,33 +1,61 @@
-
 import React from 'react';
 import { StyledColumn } from '../styles/Column.styled';
 import Task from './Task';
 import { connect } from 'react-redux';
 import { moveTaskAction } from '../actions/kanban';
+import ConfirmationDialog from './ConfirmationDialog';
 
+class Column extends React.Component {
+  state = {
+    isConfirmationOpen: false,
+    taskIdToMove: null,
+  };
 
-class Column extends React.Component {   
+  handleDrop = (event) => {
+    event.preventDefault();
+    const taskId = event.dataTransfer.getData('taskId');
+    const columnId = this.props.id;
+    const tasksInColumn = this.props.tasks.filter(
+      (task) => task.columnId === columnId
+    );
 
-  render() {
-
-    const {columnTitle, limit, id} = this.props
-
-    const handleDrop = (event) => {
-      event.preventDefault()
-      const taskId = event.dataTransfer.getData("taskId");
-      const columndId=id;
-      this.props.moveTask(Number(taskId),columndId)
+    if (tasksInColumn.length >= this.props.limit) {
+      this.setState({
+        isConfirmationOpen: true,
+        taskIdToMove: taskId,
+      });
+      return;
     }
 
+    this.props.moveTask(Number(taskId), columnId);
+  };
+
+  handleCancel = () => {
+    this.setState({
+      isConfirmationOpen: false,
+    });
+  };
+
+  render() {
+    const { columnTitle, limit} = this.props;
+    const { isConfirmationOpen } = this.state;
+
     return (
-      
-      <StyledColumn  onDrop={handleDrop} onDragOver={(event) => event.preventDefault()}>
+      <StyledColumn onDrop={this.handleDrop} onDragOver={(event) => event.preventDefault()}>
         <header>
           <h2>{columnTitle}</h2>
           <p>Tasks limit: {limit}</p>
         </header>
-        {this.props.tasks.filter(task => task.columnId === id).map(task => 
-          <Task id={task.id} title={task.title} description={task.description} member={task.member} avatar={task.avatar} priority={task.priority} theme={task.theme}/> )}
+        {this.props.tasks
+          .filter((task) => task.columnId === this.props.id)
+          .map((task) => (
+            <Task id={task.id} title={task.title} description={task.description} member={task.member} avatar={task.avatar} priority={task.priority} theme={task.theme}/>
+          ))}
+        {isConfirmationOpen && (
+          <ConfirmationDialog type="limit-info" onCancel={this.handleCancel} isOpen={isConfirmationOpen}
+            message={`Cannot move task to column ${this.props.id}. Limit of ${limit} tasks has been reached.`}
+          />
+        )}
       </StyledColumn>
     );
   }
@@ -36,11 +64,12 @@ class Column extends React.Component {
 const mapStateToProps = (state) => {
   return {
     tasks: state.tasks,
-  }
-}
+  };
+};
 
-const mapDispatchToProps = dispatch => ({
-  moveTask: (taskId,columndId) => dispatch(moveTaskAction(taskId,columndId)),
+const mapDispatchToProps = (dispatch) => ({
+  moveTask: (taskId, columnId) =>
+    dispatch(moveTaskAction(taskId, columnId)),
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(Column)
+export default connect(mapStateToProps, mapDispatchToProps)(Column);
