911f0eb44cfee7367e47a2d3864b1357bfc51138
Anna <krolikowska.ann@gmail.com>
2023-04-11 19:41:01 +0200

    redux refactoring

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
-import reducers from './reducers'
+import rootReducer from "./redux/rootReducer";
 
-const store = createStore(reducers);
+const store = createStore(rootReducer);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -17,7 +17,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
