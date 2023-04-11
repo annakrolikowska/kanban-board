701a592098c9e09bf5b7454fb76b8a067edac40b
Anna <krolikowska.ann@gmail.com>
2023-04-11 13:52:02 +0200

    created css variables

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -5,10 +5,31 @@ const GlobalStyle = createGlobalStyle`
 
     ${reset}
 
+    :root {
+        --color-primary: #E9E7F2;
+        --color-secondary: #F8F7F7;
+        --color-accent: #7B54CE;
+        --color-accent-hover: #9576D8;
+        --color-priority-low: #BFFBD3;
+        --color-priority-medium: #FFE3AC;
+        --color-priority-high: #FFACAC;
+        --color-theme-orange: #FCB36E;
+        --color-theme-green: #5CB16D;
+        --color-theme-blue: #5E8AF1;
+        --color-theme-pink: #EF7DCF;  
+        --color-white: #fff;
+        --color-black: #000;
+        --color-grey: #909090;
+        --color-light-grey: #B5B5B5;
+    
+        --font-primary: 'Montserrat', sans-serif;
+    }
+
+
     body {
         max-width: 1440px;
         margin: 0 auto;
-        font-family: 'Montserrat', sans-serif;
+        font-family: var(--font-primary)
     }
 `;
  
